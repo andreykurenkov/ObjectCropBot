c2db5882d45c5fab734a2f3c040ce0f6755facf6
Andrey Kurenkov <andreyvkurenkov@gmail.comx>
2016-12-01 19:55:34 +0000

    Small changes

--- DataSampler.lua
+++ DataSampler.lua
@@ -133,7 +133,7 @@ function DataSampler:calcDistanceInp(imgInp, lbl, gSz, wSz)
   clickIdx = math.floor(idx[cropClick])
   cropClickX = math.floor((clickIdx%gSz+1)*wSz/gSz)
   cropClickY = math.floor(math.floor(clickIdx/gSz+1)*wSz/gSz)
-  imgInp[{{1,3},cropClickX,cropClickY}] = torch.Tensor({0,0,0})
+  imgInp[{{1,3},cropClickX,cropClickY}] = torch.Tensor({0.111,0.222,0.333})
 
   -- Calculate location difference from click pixel, via 2 norm
   pixels = torch.Tensor(torch.linspace(1,wSz,wSz))
