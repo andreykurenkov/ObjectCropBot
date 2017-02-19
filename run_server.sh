8bb3d7f18125bea3020f05b278a05e3c52252da4
Andrey Kurenkov <andreyvkurenkov@gmail.comx>
2017-02-19 14:11:35 +0000

    A few tweaks

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,4 @@
-th computePatchProposalServer.lua pretrained/sharpmask
+until $(th computePatchProposalServer.lua); do
+    echo "Sharpmask server crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
